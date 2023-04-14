d8be1b69d387eeb8375d6ab71654438cac5c3567
Toby Smith <=>
2023-04-14 11:31:16 +0100

    Revert "Added tests"
    This reverts commit 8d845b01859fd2d3d1bc8100089e0c7d264f4441.

--- src/common/authentication.ts
+++ src/common/authentication.ts
@@ -1,15 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { RequestHandler, Response, Request } from 'express';
-import config from 'config';
 
 export const authenticateToken: RequestHandler = async (req: Request, res: Response, next) => {
   try {
-    //Pass if this is a test
-    if (config.get('test') === true) {
-      next();
-      return;
-    }
-
     const authHeader = req.headers['authorization'] as string;
     const token = authHeader?.split(' ')[1];
     if (!token || token === null) return res.status(401).json('Please add a authorisation token');
